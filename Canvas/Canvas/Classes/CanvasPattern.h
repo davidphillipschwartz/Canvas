e5a8ab7234bcb419a69312ca69b1d29d011ec49f
davidphillipschwartz <davidphillipschwartz@gmail.com>
2014-01-12 17:34:49 -0800

    working on CanvasPattern. fun with malloc()!

--- Canvas/Canvas/Classes/CanvasPattern.h
+++ Canvas/Canvas/Classes/CanvasPattern.h
@@ -2,12 +2,24 @@
 //  CanvasPattern.h
 //  Canvas
 //
-//  Created by Malcolm Levy on 2014-01-12.
+//  Created by David Schwartz on 2014-01-12.
 //  Copyright (c) 2014 Hybridity. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "CanvasArrayView.h"
+
+typedef struct
+{
+    float r, g, b;
+} CanvasPixel;
 
 @interface CanvasPattern : NSObject
+{
+    int width, height, length;
+    CanvasPixel* pixelArray;
+}
+
+- (id)initWithWidth:(int)_width Height:(int)_height Length:(int)_length;
 
 @end
--- Canvas/Canvas/Classes/CanvasPattern.m
+++ Canvas/Canvas/Classes/CanvasPattern.m
@@ -2,12 +2,35 @@
 //  CanvasPattern.m
 //  Canvas
 //
-//  Created by Malcolm Levy on 2014-01-12.
+//  Created by David Schwartz on 2014-01-12.
 //  Copyright (c) 2014 Hybridity. All rights reserved.
 //
 
 #import "CanvasPattern.h"
 
 @implementation CanvasPattern
+
+- (id)init
+{
+    // incomplete
+    return nil;
+}
+
+- (id)initWithWidth:(int)_width Height:(int)_height Length:(int)_length
+{
+    self = [super init];
+    if (self) {
+        width = _width;
+        height = _height;
+        length = _length;
+        pixelArray = malloc(width*height*length*sizeof(CanvasPixel));
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    free(pixelArray);
+}
 
 @end
